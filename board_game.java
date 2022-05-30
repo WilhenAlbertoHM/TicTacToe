ee3081f63a408e5f66e8e6208cc4d599fcee4059
Wilhen Alberto Hui Mei <92064680+WilhenAlbertoHM@users.noreply.github.com>
2022-05-30 19:03:27 -0400

    Update board_game.java

--- board_game.java
+++ board_game.java
@@ -4,16 +4,6 @@ import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-/*
-    Inspired by YouTube videos regarding the Tic Tac Toe board game.
-    The programs assimilates the board game, consisting of user input in the terminal. Then, the user will get asked to type a number from 1 to 9 and,
-    depending on the number, the user will place a 'X' on the number, and the cpu will automatically place 'O' randomly.
-    The game board has three rows and three columns, each "slot" corresponding the numbers from 1 to 9. The program will ask the user to
-    put a number until the game ends in a win, defeat, or draw. Also, the user will play against the computer and this program can be
-    improved if AI is implemented. However, this is a simple program.
-
-*/
-
 public class board_game {
     // Keep track of player and cpu's positions.
     static ArrayList<Integer> playerPos = new ArrayList<>();
