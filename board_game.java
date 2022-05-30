f1bc294ab09bbd6cfd2e14237d3aa799bc7cde55
Wilhen Alberto Hui Mei <92064680+WilhenAlbertoHM@users.noreply.github.com>
2022-05-30 19:04:10 -0400

    Update board_game.java

--- board_game.java
+++ board_game.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class board_game {
     // Keep track of player and cpu's positions.
